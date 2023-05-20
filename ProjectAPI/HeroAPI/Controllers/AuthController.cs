2e7214c537d652ae5de81ebe7c6b581b2472d94a
Phanyapat Chalermchai <81134953+mlpokniwza@users.noreply.github.com>
2023-05-20 09:25:22 +0700

    add login

--- ProjectAPI/HeroAPI/Controllers/AuthController.cs
+++ ProjectAPI/HeroAPI/Controllers/AuthController.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
 using HeroAPI.Data;
 using HeroAPI.DTO;
 using HeroAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace HeroAPI.Controllers
 {
@@ -15,15 +19,21 @@ namespace HeroAPI.Controllers
     {
         private readonly IAuthRepository _repo;
 
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
         {
             // validate username and password
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if (await _repo.UserExists(userForRegisterDto.Username))
@@ -34,6 +44,43 @@ namespace HeroAPI.Controllers
             var createUser = await _repo.Register(userToCreate, userForRegisterDto.Username);
 
             return StatusCode(201);
+        }
+        
+        [HttpPost("login")]
+        public async Task<IActionResult> Register([FromBody]UserForLoginDto userForLoginDto)
+        {
+            //check user matches
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+            // create security key
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            // encrypt key with hashing algorithm
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+            
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new{
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
